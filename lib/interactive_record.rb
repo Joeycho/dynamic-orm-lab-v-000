80d8f67ca740bafa04d73e6a578ab6324a03e30d
Joey Cho <cjy12182000@naver.com>
2018-08-08 08:21:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
 
   def self.column_names
     DB[:conn].results_as_hash = true
-    binding.pry
+
     sql = "PRAGMA table_info(`#{table_name}`)"
 
     table_info = DB[:conn].execute(sql)
