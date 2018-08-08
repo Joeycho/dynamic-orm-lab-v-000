7d17c22b04185b79ef4049fb313286b059a54573
Joey Cho <cjy12182000@naver.com>
2018-08-08 09:24:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -63,7 +63,7 @@ class InteractiveRecord
   DB[:conn].execute(sql,name)
   end
 
-  def self.find_by(attribute: value)
+  def self.find_by(value)
   binding.pry
   sql = <<-SQL
   SELECT * FROM #{self.table_name} WHERE#{attribute.to_sym}  = ?
