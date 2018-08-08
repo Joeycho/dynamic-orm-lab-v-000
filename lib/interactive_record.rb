735f9bf30c44d353c1d5cdf6809da70cc6f60789
Joey Cho <cjy12182000@naver.com>
2018-08-08 08:54:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,4 +39,15 @@ class InteractiveRecord
     self.class.column_names.delete_if{|col| col == "id"}.join(", ")
   end
 
+  values = []
+
+  def self.class.column_names.each do
+    |col_name|
+    values << "'#{send(col_name)}'"
+  unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+  
+
 end
